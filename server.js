c25872f57371766158d510812a926e15cc323daa
Benoît Gelineau <39692884+benoitgelineau@users.noreply.github.com>
2018-07-07 20:51:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const shortid = require('shortid');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const indexRouter = require('./routes/index');
 
-const cors = require('cors');
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-mongoose
-  .connect(
-    process.env.MLAB_URI || 'mongodb://localhost/exercise-track',
-    { useMongoClient: true },
-    err => {
-      if (err) {
-        console.log(`MongoDB could not connect: ${err}`);
-      } else {
-        console.log('MongoDB connected');
-      }
-    }
-  );
+const mongoDB = process.env.MLAB_URI || "mongodb://localhost/exercise-track";
+mongoose.connect(mongoDB, { useMongoClient: true });
 
-const Schema = mongoose.Schema;
-const schema = new Schema({
-  _id: {
-    type: String
-    // default: shortid.generate (to be used in case of Promises in post method)
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  exercises: [
-    {
-      _id: {
-        type: String,
-        default: shortid.generate
-      },
-      description: {
-        type: String,
-        required: true
-      },
-      duration: {
-        type: Number,
-        required: true
-      },
-      date: {
-        type: String
-      }
-    }
-  ]
-});
+mongoose.Promise = global.Promise;
 
-const User = mongoose.model('User', schema);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, 'MongoDB could not connect:'));
 
 app.use(cors());
 
@@ -63,112 +26,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// New user
-app.post('/api/exercise/new-user', (req, res, next) => {
+app.use('/api/exercise', indexRouter);
 
-  req.id = shortid.generate();
-  req.name = req.body.username.toLowerCase();
-  
-  next();
-}, (req, res) => {
-  
-  // Create new user if username is not taken
-  User.findOne({ username: req.name }, (err, result) => {
-    if (err) console.log(`Error in finding username: ${err}`);
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
 
-    if (result) {
-      res.type("txt").send("This username has already been taken.");
-      
-    } else {
-
-      User.create({ _id: req.id, username: req.name }, (err, user) => {
-
-        if (err) {
-          res.type("txt").send('Please insert a username.');
-
-        } else {
-          res.json({ username: user.username, _id: user._id });
-          console.log(`New user created: ${user}`);
-        }
-      });
-    }
-  });
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
 });
-
-const init = (req, res, next) => {
-  req.id = req.body.userId;
-  req.descr = req.body.description;
-  req.dur = parseInt(req.body.duration);
-
-  next();
-}
-
-const isDateValid = (req, res, next) => {
-  req.date = req.body.date ? new Date(req.body.date) : new Date(); // Should use default value in schema
-  req.dateValid = req.date.getTime() ? true : false;
-
-  next();
-};
-
-// New exercise
-app.post('/api/exercise/add', init, isDateValid, (req, res) => {
-
-  User.findById(req.id, (err, user) => {
-
-    // Handle responses
-    if (!user) {
-      res.type('txt').send('No user found with this id.');
-
-    } else if (!req.descr || !req.dur) {
-      res.type('txt').send('Please fill in all the information required.');
-
-    } else if (!req.dateValid) {
-      res.type("txt").send("Please enter a valid date.");
-
-    } else {
-
-      user.exercises.push({
-        description: req.descr,
-        duration: req.dur,
-        date: req.date.toDateString()
-      });
-
-      user.save((err, user) => {
-        res.json({
-          _id: user._id,
-          username: user.username,
-          description: req.descr,
-          duration: req.dur,
-          date: req.date.toDateString()
-        });
-      });
-    }
-  });
-});
-
-// // Not found middleware
-// app.use((req, res, next) => {
-//   return next({ status: 404, message: 'not found' })
-// });
-
-// // Error Handling middleware
-// app.use((err, req, res, next) => {
-//   let errCode, errMessage
-
-//   if (err.errors) {
-//     // mongoose validation error
-//     errCode = 400 // bad request
-//     const keys = Object.keys(err.errors)
-//     // report the first validation error
-//     errMessage = err.errors[keys[0]].message
-//   } else {
-//     // generic or custom error
-//     errCode = err.status || 500
-//     errMessage = err.message || 'Internal Server Error'
-//   }
-//   res.status(errCode).type('txt')
-//     .send(errMessage)
-// });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
